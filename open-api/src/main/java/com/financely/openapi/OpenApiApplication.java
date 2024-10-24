a340ac51124e4eb82cc93c6764e2938e145988d5
orierezbluestein <orierezblu@gmail.com>
2024-10-25 01:21:20 +0300

    FINANCELY FIRST COMMIT

---
+++ open-api/src/main/java/com/financely/openapi/OpenApiApplication.java
@@ -0,0 +1,15 @@
+package com.financely.openapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OpenApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenApiApplication.class, args);
+	}
+
+}
